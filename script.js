c7ab9f78415309486f183ff203f8a4092f567d29
amr <nazurdinov95>
2019-12-21 14:39:31 +0500

    add method forEach

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let num = [123, 2, 104];
 let sum = 0;
 
-for (let i = 0; i < num.length; i++) {
-  sum += num[i].toString().split('').map(Number)
-      .reduce((res, el) => res + el,);
-}
-console.log(sum);
+num.toString().split('').map(Number).forEach((number) => sum += number);
 
+console.log(sum);
