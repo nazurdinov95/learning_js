db96916bc7695062f3b31cde1a4553df36e0e889
amr <nazurdinov95>
2019-12-22 22:12:25 +0500

    add method for

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+
 let strings = ['Amr', 'Jimmy'];
 let sum = 0;
 
-strings.forEach(function(elem) {
+strings.forEach((elem) => {
   sum += elem.length;
 });
 console.log(sum);
+
+let stringArry = ['Hello', 'World'];
+
+  let sumStr = 0;
+  for(let i = 0; i < stringArry.length; i++){
+    sumStr += stringArry[i].length;
+  }
+  console.log(sumStr);
