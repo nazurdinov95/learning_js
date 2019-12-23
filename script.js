92f8329653d554ec3b0e52fbbf5f85f15be72fe3
amr <nazurdinov95>
2019-12-23 12:13:36 +0500

    add length every element method map

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//sum all of elements array
 let strings = ['Amr', 'Jimmy'];
 let sum = 0;
 
@@ -6,7 +7,7 @@ strings.forEach((elem) => {
 });
 console.log(sum);
 
-//sum length element
+//sum all of elements array
 let stringArry = ['Hello', 'World'];
 
 let sumStr = 0;
@@ -18,6 +19,12 @@ console.log(sumStr);
 //length every element
 const emotions = ['happy', 'sad', 'angry'];
 let arr = [];
-emotions.forEach(emotion => (arr.push(emotion.length)));
+emotions.forEach(emotion => arr.push(emotion.length));
 
 console.log(arr);
+//length every element array method map
+let stringsArr2 = ['Hi', 'jimmy'];
+let lengthStr = stringsArr2.map(num => {
+  return num.length;
+});
+console.log(lengthStr);
