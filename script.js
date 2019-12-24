41b890c86d8a2fc948409f0f5d44eb945afbe7a0
amr <nazurdinov95>
2019-12-24 22:44:14 +0500

    add something

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numArray = [1, 4, 6, 7, 1, 0, -3, 0, 1, 2, 34, 5, 49];
+const numArray = [49, 1, 4, 6, 7, 1, 0, -3, 0, 1, 2, 34, 5];
 
 const min = Math.min.apply(null, numArray);
 const max = Math.max.apply(null, numArray);
@@ -6,10 +6,13 @@ const max = Math.max.apply(null, numArray);
 const indexMin = numArray.indexOf(min);
 const indexMax = numArray.indexOf(max);
 
-const removed = numArray.slice(indexMin, indexMax);
+if (indexMax < indexMin) {
+  let removed2 = numArray.slice(indexMax, indexMin);
+  let result = removed2.reduce((sum, num) => sum + num, 0);
+  console.log(result - max);
+} else {
+  const removed = numArray.slice(indexMin, indexMax);
+  let sumArray = removed.reduce((sum,num) => sum + num);
 
-let sumArray = 0;
-
-removed.forEach((num) => sumArray += num);
-
-console.log(sumArray - min);
+  console.log(sumArray - min);
+}
