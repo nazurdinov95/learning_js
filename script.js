f6be7c3e6d8e1f02d1db70b5107185add19b143a
amr <nazurdinov95>
2019-12-24 21:53:15 +0500

    add project

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+const numArray = [1, 4, 6, 7, 1, 0, -3, 0, 1, 2, 34, 5, 49];
 
-let numArray = [1, 4, 6, 7, 1, 0, -3, 0, 1, 2, 34, 5, 49];
+const min = Math.min.apply(null, numArray);
+const max = Math.max.apply(null, numArray);
 
-let sumArray =0;
+const indexMin = numArray.indexOf(min);
+const indexMax = numArray.indexOf(max);
 
-  numArray.forEach((num) => sumArray +=num);
+const removed = numArray.slice(indexMin, indexMax);
 
-  let max = Math.max.apply(null, numArray);
-  let min = Math.min.apply(null,numArray);
-  let sumMinMax = max + min;
-    sumArray -= sumMinMax;
+let sumArray = 0;
 
+removed.forEach((num) => sumArray += num);
 
-  console.log(sumArray);
+console.log(sumArray - min);
