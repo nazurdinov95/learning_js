9307bcf635b625471eee55fe51939b1de41c1324
amr <nazurdinov95>
2019-12-25 20:22:02 +0500

    add project

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-const numArray = [49, 1, 4, 6, 7, 1, 0, -3, 0, 1, 2, 34, 5];
+let numArray = [49, 1, 4, 6, 7, 1, 0, -3, 0, 1, 2, 34, 5];
 
-const min = Math.min(... numArray);
-const max = Math.max(... numArray);
+const minNum = Math.min(... numArray);
+const maxNum = Math.max(... numArray);
 
-const indexMin = numArray.indexOf(min);
-const indexMax = numArray.indexOf(max);
-let removed;
-if (indexMax < indexMin) {
-  removed = numArray.slice(indexMax+1, indexMin);
-} else {
-  removed = numArray.slice(indexMin, indexMax);
-}
+const indexMin = numArray.indexOf(minNum);
+const indexMax = numArray.indexOf(maxNum);
 
-let sumArray = removed.reduce((sum,num) => sum + num);
+let startIn = numArray[indexMin];
 
-console.log(sumArray);
+numArray[indexMin] = numArray[indexMax];
+numArray[indexMax] = startIn;
+
+console.log(numArray);
