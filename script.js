ca5005d914e264cd8618e5bb516912fd4a48eecc
amr <nazurdinov95>
2020-01-01 09:40:12 +0500

    change something

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let numbers = [12, 5, 1, 3, 1];
+const numbers = [12, 5, 1, 3, 1];
 
-    unique = numbers.filter((v, i) => numbers.indexOf(v) === numbers.lastIndexOf(v));
+unique = numbers.filter((v, i) => numbers.indexOf(v) === numbers.lastIndexOf(v));
 
 console.log(unique);
