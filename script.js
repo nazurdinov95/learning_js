e9c60cdc8838f49e6dd9bdccc602672a4dcc2eea
amr <nazurdinov95>
2020-01-08 22:17:44 +0500

    add project

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const numbers = [3, 6, 20, 99, 10, 15];
-const median = arr => {
-  const mid = Math.floor(arr.length / 2),
-      nums = [...arr].sort((a, b) => a - b);
-  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
-};
-console.log(median(numbers));
+let numeral = parseInt(document.getElementById('text-input').value);
+
+const numeralCodes = [["","I","II","III","IV","V","VI","VII","VIII","IX"],         // Ones
+  ["","X","XX","XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],   // Tens
+  ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]];        // Hundreds
+
+function convert(num) {
+  numeral = "";
+  let digits = num.toString().split('').reverse();
+  for (let i=0; i < digits.length; i++){
+    numeral = numeralCodes[i][parseInt(digits[i])] + numeral;
+  }
+  return numeral;
+}
+
+document.getElementById('text-output').value = convert(numeral);
